68e5ad7381e8df08a450de5a28cdec1bb5f6e7c9
unknown <C:\Users\Lx-987\AppData\Roaming\The Bat!>
2021-09-02 21:24:14 +0300

    установка опенкарта

---
+++ admin/config.php
@@ -0,0 +1,37 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost4/admin/');
+define('HTTP_CATALOG', 'http://localhost4/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost4/admin/');
+define('HTTPS_CATALOG', 'http://localhost4/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/localhost4/admin/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/localhost4/system/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/localhost4/image/');
+define('DIR_STORAGE', 'C:/OpenServer/domains/localhost4/storage/');
+define('DIR_CATALOG', 'C:/OpenServer/domains/localhost4/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'localhost4');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
+define('OPENCARTFORUM_SERVER', 'https://opencartforum.com/');
---
+++ storage/cache/13/1387aaad97ea893d17df2974c962cd4e1355b6f515734b663169145ba5fde5af.php
@@ -0,0 +1,49 @@
+<?php
+
+/* default/template/common/content_top.twig */
+class __TwigTemplate_d71ceace0cc8842f9b6377f9e9bf576d687b7a42effe7e878a403bfb1683d922 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["modules"]) ? $context["modules"] : null));
+        foreach ($context['_seq'] as $context["_key"] => $context["module"]) {
+            // line 2
+            echo $context["module"];
+            echo "
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['module'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+    }
+
+    public function getTemplateName()
+    {
+        return "default/template/common/content_top.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  23 => 2,  19 => 1,);
+    }
+}
+/* {% for module in modules %}*/
+/* {{ module }}*/
+/* {% endfor %}*/
